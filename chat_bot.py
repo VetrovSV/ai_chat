9da6f000005cad09bb042da5fcaeb8c5760110bd
NineTrier <ramenskiy.2015@mail.ru>
2024-06-02 10:17:50 +0900

    Убрал лишнее

--- chat_bot.py
+++ chat_bot.py
@@ -26,15 +26,11 @@ def init_emb_model(model_name:str, model_kwargs:dict, encode_kwargs:dict):
     """Скачивает (если нужно) языковую модель для эмбеддингов, возвращает её"""
     print("Создание\скачивание модели эмбеддингов текстов... ", end="")
     # загрузка модели эмбеддингов
-    # model_kwargs = {'device': 'cpu'}
-    # encode_kwargs = {'normalize_embeddings': False}
-    # сделать более точную настройку параметров, если необходимо
     Embeddings_maker = HuggingFaceEmbeddings(
         model_name=model_name,
         model_kwargs=model_kwargs,
         encode_kwargs=encode_kwargs
     )
-    # todo: копировать уже скаченную модель в докер
     print("Готово")
     return Embeddings_maker
 
@@ -72,10 +68,10 @@ def get_context(user_request: str, db, top):
     @param top - сколько похожих объектов извлекать?
     #@return контекст (текст со встроенными ссылками), список ссылок
     """
-    # todo: добавлять ссылку
-    # todo: сделать отбор документов для контекста на основе порогового расстояния?
+    # Запрашиваем дополнительный контекст для RAG
     context = db.similarity_search_with_score(user_request, k=top)
     print(context)
+    # Берём ближайший вектор
     nearest = min(context, key=lambda x: x[1])
     # Если ближайший вектор очень близок, то это в точности тот вопрос, значит можно вернуть его описание и ссылку на статью
     # Статус 0 - ответ не очевиден, стоит обратиться к модели
@@ -85,17 +81,12 @@ def get_context(user_request: str, db, top):
         return 1, f"{nearest[0].metadata['description']}. Подробнее по ссылке {nearest[0].metadata['url']}", [nearest[0].metadata['url']]
     if nearest[1] > 0.71:
         return 2, "", []
-    # print(context)
-    # идекс 0 - документ
-    # идекс 1 - похожесть
+    # индекс 0 - документ
+    # индекс 1 - похожесть
     context_text = "\n\n".join([ "Вопрос: " + text[0].page_content +
                             "\nОтвет: " +  text[0].metadata['description'] +
                            "\nСсылка: " + text[0].metadata['url'] for text in context])
     links = [text[0].metadata['url'] for text in context]
-    # description - ответ
-    # title - вопрос
-    # todo: контролировать размер контекста, чтобы он влезал в промпт LLM
-    # print(context)
     return 0, context_text, links
 
 
@@ -105,14 +96,8 @@ def init_DB():
     Embeddings_maker = init_emb_model(model_name=EMB_MODEL_NAME,
                                                model_kwargs={'device': 'cpu'},
                                                encode_kwargs={'normalize_embeddings': False})
-    # попробовать нормализацию эмбеддингов?
-
-    # Texts = load_dataset(filename_json="data/dataset.json", embeddings_maker=Embeddings_maker)
     # создаем хранилище
     print("создаем хранилище... ", end="")
-    # DB = FAISS.from_documents(Texts, Embeddings_maker)    # вернёт экземпляр FAISS
-    # DB.save_local(DB_FAISS)
-    # https://api.python.langchain.com/en/latest/vectorstores/langchain_community.vectorstores.faiss.FAISS.html#langchain_community.vectorstores.faiss.FAISS.save_local
     DB = FAISS.load_local(folder_path=DB_FAISS, embeddings=Embeddings_maker,
                           allow_dangerous_deserialization=True  # да, я уверен, что в файле нет вредоносного кода
                           )
@@ -120,9 +105,13 @@ def init_DB():
     return DB
 
 def get_Answer_from_YAGPT(text):
+    """Реализация работы с YandexGPT"""
     global DB
+    # Получаем состояние работы, контекс и ссылки
     state, context, links = get_context(text, DB, top=2)
+    # Нулевое состояние, значит прямого ответа нет, нужно сделать запрос к модели
     if state == 0:
+        # Запрос к YandexGPT Pro
         req = {
                 "modelUri": "ds://bt168m74v9ui1ml0upnh",
                 "completionOptions": {
@@ -141,38 +130,18 @@ def get_Answer_from_YAGPT(text):
                     }
                 ]
         }
+        # Заголовки запроса
         headers = {"Authorization" : "Api-Key " + 'AQVNz9fpuAtA5fGeDB_rcMEY_byJDgEEPeivMMYn', "x-folder-id": "b1g72uajlds114mlufqi", }
+        # Ответ на запрос
         res = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion", headers=headers, json=req)
-        print(res)
         res = res.json()
         return res['result']['alternatives'][0]['message']['text'], links
+    # Первое состояние, значит прямого ответа есть и можно выдать сразу его, чтобы не занимать время на обработку моделью
     elif state == 1:
         return context, links
+    # Второе состояние, значит ответа в базе знаний нет и стоит ответить, что ответа нет
     elif state == 2:
         return "У меня нет ответа на этот вопрос.", []
-# нужно перенести в более подходящее место
+
+
 DB = init_DB()
-
-# def check_some_context(db, top):
-#     """Получить контекст для вопроса (top - число документов) используя БД db
-#     @param user_request: исходный запрос пользователя
-#     @param db - объект векторной БД
-#     @param top - сколько похожих объектов извлекать?
-#     #@return контекст (текст со встроенными ссылками), список ссылок
-#     """
-#     # todo: добавлять ссылку
-#     # todo: сделать отбор документов для контекста на основе порогового расстояния?
-#     with open('data.txt', encoding='utf-8') as f:
-#         data = {'question': [], 'data_question':[], 'answer':[], 'distance': []}
-#         for text in f.readlines():
-#             context = db.similarity_search_with_score(text, k=top)
-#             print(context[0][0])
-#             data['question'].append(text)
-#             data['data_question'].append(context[0][0].page_content)
-#             data['answer'].append(context[0][0].metadata['description'])
-#             data['distance'].append(context[0][1])
-#         d = pd.DataFrame.from_dict(data)
-#         print(d)
-#         d.to_excel('data_bad.xlsx')
-
-# check_some_context(DB, 1)
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 """
-Главный файл приложения. Криво добавлен сервер (см. todo ниже)
-Для простоты отвечает только контекстными документами.
-Важно чтобы этот файл назывался main.py!
+Главный файл приложения.
 """
 
 from fastapi import FastAPI, HTTPException
@@ -14,11 +12,11 @@ import requests
 print("Создание сервера")
 app = FastAPI(title="Assistant API", version="0.1.0")
 
-
+# Запрос assist
 @app.post("/assist", response_model=Response, responses={422: {"model": HTTPValidationError}})
 async def assist(request: Request):
-    # global chat_bot.DB
     print(f"Вопрос: {request.query}")
+    # Получение ответа от чат бота
     text, links = chat_bot.get_Answer_from_YAGPT(request.query)
     print(f"Ответ: {text}")
     return Response(text=text, links=links)
@@ -27,13 +25,7 @@ async def assist(request: Request):
 if __name__ == "__main__":
     print("Запуск сервера")
     # uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
-    HOST = "localhost"
+    HOST = "0.0.0.0"
     PORT = 60004                        
     print(f"Запуск на {HOST}:{PORT}")
     uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
-
-#
-# 1. Запуск ollama serve
-# 2. Запуск этого файла
-# 3. проверка
-# curl -X POST -H 'Content-Type: application/json' -d '{"query":"Как мне получить кредит?"}'  http://0.0.0.0:60004/assist
